456a4a9539bdf61cc980e85aae938a452e685f6a
Hobgoblin1123 <86826255+Hobgoblin1123@users.noreply.github.com>
2022-01-07 16:52:37 +0900

    Add files via upload

---
+++ homepage/assets/main.js
@@ -0,0 +1,5 @@
+$(function () {
+  $('.Ibtn').on('click', () => {
+      $('.important').fadeToggle();
+  });
+});
---
+++ homepage/assets/shutter.js
@@ -0,0 +1,18 @@
+// $(function(){
+//  $('#first').css({'z-index':'1000'});
+//   setTimeout(function(){
+//     $('#second').css({'z-index':'1001'});
+//   },2000);
+//   setTimeout(function(){
+//     $('.shutter img').fadeOut(500);
+//   },5000);
+// })
+
+$(function(){
+  setTimeout(function(){
+    $('.shutter h1').fadeIn(2000);
+  },500)
+  setTimeout(function(){
+    $('.shutter').fadeOut(1000);
+  },3000)
+})
